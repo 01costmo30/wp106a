cccacdbe2f6493fb56f10604ef3db63da1059de9
01costmo30 <31913793+01costmo30@users.noreply.github.com>
2017-09-27 23:44:31 +0800

    Add files via upload

--- exercise/b/main.js
+++ exercise/b/main.js
@@ -9,7 +9,7 @@ let win
 function createWindow () {
   // Create the browser window.
 //  win = new BrowserWindow({width: 100, height: 50, frame: false})
-  win = new BrowserWindow({width: 100, height: 80, frame: true})
+  win = new BrowserWindow({width: 150, height: 200, frame: true})
   win.setMenu(null)
 
   // and load the index.html of the app.
